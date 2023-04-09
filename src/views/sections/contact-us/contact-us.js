8e559ac9d429ec1986aff69665ac861faeddb154
katie-lee-pointer <katie.lee9912@gmail.com>
2023-04-09 18:20:59 -0400

    design qa revision and fixes

--- src/views/sections/contact-us/contact-us.js
+++ src/views/sections/contact-us/contact-us.js
@@ -1,1 +1,18 @@
 import './contact-us.scss'
+(() => {
+  if (typeof window.customElements.get('contact-us') !== 'undefined') { return }
+  window.customElements.define('contact-us', class ContactUs extends HTMLElement { // eslint-disable-line
+    constructor () {
+      super()
+
+      this.badge = this.querySelector('[data-lets-chat-badge]')
+      this.initBadge()
+    }
+
+    initBadge () {
+      document.addEventListener('scroll', () => {
+        this.badge.style.transform = 'rotate(' + (window.scrollY * 0.1) + 'deg)'
+      })
+    }
+  })
+})()
